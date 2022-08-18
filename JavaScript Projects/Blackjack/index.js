7d75f2388295abcd6638335f89cf9c4fc51120a8
Marcelo <maarcelo.bjr@gmail.com>
2022-08-18 19:17:25 -0300

    Blackjack

---
+++ JavaScript Projects/Blackjack/index.js
@@ -0,0 +1,47 @@
+let firstCard = 10
+let secondCard = 11
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame(){
+    renderGame();
+}
+
+function renderGame(){
+
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    
+    sumEl.textContent = "Sum: " + sum;
+
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+}
+
+function newCard(){
+
+    messageEl.textContent = "Drawing a new card!"; 
+
+    let newC = 6;
+    
+    sum = sum + newC;
+
+    startGame(); 
+}
+
